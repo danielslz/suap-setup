7597bc7b0c96909744d8b0ba697d89c8e46af129
Daniel Lima <danielslz@gmail.com>
2026-06-30 10:52:39 -0300

    Correção placeholder flower

--- supervisor/run_celery_flower.sh
+++ supervisor/run_celery_flower.sh
@@ -6,7 +6,7 @@ SUAP_DIR=$BASE_DIR/suap
 VENV_DIR=$BASE_DIR/venv
 
 CELERY_BROKER_URL=${CELERY_BROKER_URL:-"redis://192.168.1.100:6379/3"}
-FLOWER_BASIC_AUTH=${CELERY_FLOWER_AUTH:-"admin:##mysupersecretpassword##"}
+FLOWER_BASIC_AUTH=${CELERY_FLOWER_AUTH:?"ERRO: variável CELERY_FLOWER_AUTH não definida. Defina no formato 'usuario:senha'."}
 
 GREEN=`tput setaf 2`
 NO_COLOR=`tput sgr0`
